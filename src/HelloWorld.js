5c2a67177cfdea6f24795bfb3199422e0b75bf2f
AbiMA <48912566+indigosaurus@users.noreply.github.com>
2024-01-16 14:28:55 +0000

    initial commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+      <h3>Hello World <br></br>
+      This is Abi's React Boilerplate.</h3>
+  );
+};
+
+export default HelloWorld;
